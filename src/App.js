d87d6874cb9968f35f2cf3fbc8bfcef62d216882
Plumnet <tomomi0131@ymail.ne.jp>
2023-12-11 14:43:23 +0900

    バリデーション(E-mailのみ)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import { useForm } from 'react-hook-form';
 import './styles.css';
 
 export default function App() {
-		/* useFormから関数をimport */
+		/* useFormからformState: {errors} をimport */
     const {
         register,
         handleSubmit,
+				formState: { errors }
     } = useForm();
 		
     const onSubmit = (data) => console.log('onSubmit:', data);
-    
+
     return (
-        /* handleSubmitはフォームの入力内容を検証したうえで、引数に渡した関数(onSubmit)を実行 */
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName")} />
-          <input {...register("lastName")} />
-          <input {...register("nickName")} />
-          <input {...register("email")} />
-          <input {...register("age")} />
-          <input type="submit" />
-      </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+		    <label>First Name</label>
+		    {/* register関数の第2引数にバリデーションのルールを追加 */}
+		    <input {...register("firstName", { maxLength: 15 })} />
+		    {/* errorsと登録したname属性を使用して、エラーメッセージを記述 */}
+		    {errors.firstName && <p>15文字以内で入力してください</p>}
+		    <label>Last Name</label>
+		    <input {...register("lastName", { maxLength: 15 })} />
+		    {errors.lastName && <p>15文字以内で入力してください</p>}
+		    <label>Email</label>
+		    <input {...register("email", { required: true })} />
+		    {errors.email && <p>メールアドレスは必須です</p>}
+		    <input type="submit" value="submit" />
+		  </form>
     );
 }
