e59bb3cd61e9b29b288d892f7c5447dfc8c3a073
Plumnet <tomomi0131@ymail.ne.jp>
2023-12-11 14:51:41 +0900

    バリデーション(全ての項目対応)

--- src/App.js
+++ src/App.js
@@ -15,14 +15,16 @@ export default function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
 		    <label>First Name</label>
 		    {/* register関数の第2引数にバリデーションのルールを追加 */}
-		    <input {...register("firstName", { maxLength: 15 })} />
+		    <input {...register("firstName", { maxLength: 15, required: true })} />
 		    {/* errorsと登録したname属性を使用して、エラーメッセージを記述 */}
-		    {errors.firstName && <p>15文字以内で入力してください</p>}
+		    {errors.firstName?.type === "maxLength" && <p>15文字以内で入力してください</p>}
+		　  　　　　{errors.firstName?.type === "required" && <p>名は入力必須です</p>}
 		    <label>Last Name</label>
-		    <input {...register("lastName", { maxLength: 15 })} />
-		    {errors.lastName && <p>15文字以内で入力してください</p>}
+		    <input {...register("lastName", { maxLength: 15, required: true })} />
+		    {errors.lastName?.type === "maxLength" && <p>15文字以内で入力してください</p>}
+				{errors.lastName?.type === "required" && <p>性は入力必須です</p>}
 		    <label>Email</label>
-		    <input {...register("email", { required: true })} />
+		    <input {...register("email", { required: true, pattern: /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)/ })} />
 		    {errors.email && <p>メールアドレスは必須です</p>}
 		    <input type="submit" value="submit" />
 		  </form>
